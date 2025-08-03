fbf901479639388a180f3a1ef3d5dcfa1778f3f6
Rifaya09709 <rifayasafi11@gmail.com>
2025-08-03 18:37:33 +0400

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [react()],
-
-})
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    port: 3000
+  },
+  base: './',
+});
