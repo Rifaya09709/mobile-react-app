3da0859ee90033309aaf74d083351ed942692009
Rifaya09709 <rifayasafi11@gmail.com>
2025-08-03 18:25:33 +0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
-  base:process.env.VITE-BASE-PATH ||"/mobile-react-app"
+
 })
